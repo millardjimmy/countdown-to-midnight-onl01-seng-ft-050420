c528b3a562819a9e72d2fa3c8b89e8679ea2dc28
Jimmy Millard <millardjimmy@gmail.com>
2020-05-05 18:01:12 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,18 +1,16 @@
-def countdown(num)
-  while num > 0 
-    return "#{num} SECONDS(S)!"
-    num -= 1 
-  end 
-  return "HAPPY NEW YEAR!"
-end 
-
-def countdown_with_sleep(num)
-  while num > 0 
-    puts "#{num} SECOND(S)!"
-    sleep(1)
-    num -= 1 
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
   end
-  
   return "HAPPY NEW YEAR!"
-  
+end
+
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
 end
